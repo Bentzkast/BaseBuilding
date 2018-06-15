1a25d0203f788afd3ef216d190ce6f0e64d6bcd0
Joseph Alfredo <jalfredopoly@gmail.com>
2018-06-14 18:44:10 -0700

    initial commit

---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Basebuilding
+{
+	public class Tile
+    {
+
+        enum Type { Empty, Grassland };
+        Type tileType = Type.Empty;
+        
+        int x;
+		int y;
+
+		World world;
+        
+        public Tile(World world, int x, int y)
+		{
+			this.world = world;
+			this.x = x;
+			this.y = y;
+		}
+    }
+}
+
+
---
+++ Assets/Models/World.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Basebuilding
+{
+	public class World
+	{
+		Tile[,] tiles;
+		int width;
+		int height;
+
+		public World(int width = 100, int height = 100)
+		{
+			this.width = width;
+			this.height = height;
+			tiles = new Tile[width, height];
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					tiles[x, y] = new Tile(this, x, y);
+				}
+			}
+			Debug.Log("World created of size " + width + " x " + height);
+		}
+
+		public Tile GetTile(int x, int y)
+		{
+			if(x < 0 || x > width || y < 0 || y > height)
+			{
+				Debug.LogError("Invalid tile coordinate (" + x + "," + y + ")");
+				return null;
+			}
+			return tiles[x, y];
+		}
+    }
+}
+
+
